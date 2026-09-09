63c0c8c90fdad316acbb102d0b599ca643d9553d
Buddhima Emashi <buddhimasanduni3384@gmail.com>
2026-09-09 20:50:11 +0530

    completed [37123]

--- lib/main.dart
+++ lib/main.dart
@@ -26,18 +26,38 @@ class Home extends StatelessWidget {
         // TODO 2: Distribute the horizontal space evenly.
         // - Add the `mainAxisAlignment` property.
         // - Set it to MainAxisAlignment.spaceEvenly.
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
         
         // TODO 3: Align the children to the bottom (end) of the cross axis.
         // - Add the `crossAxisAlignment` property.
         // - Set it to CrossAxisAlignment.end.
+        crossAxisAlignment: CrossAxisAlignment.end,
         
         children: [
           // TODO 1: Replace the placeholder text below with three specific widgets:
           // 1. A Text widget saying "Hello World"
+          const Text(
+            "Hello World",
+          style: TextStyle(fontSize: 20),
+          ),
+            
           // 2. An ElevatedButton saying "Click Me" with a red background color.
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red,
+            ),
+            child: const Text("Click Me"),
+          ),
           // 3. A Container with a cyan background, padding of EdgeInsets.all(30), and a child Text saying "Inside Container"
+          Container(
+            padding: const EdgeInsets.all(30),
+            color:Colors.cyan,
+            child: const Text("Inside Container"),
+          ),
           
-          const Text("Replace me with the 3 widgets!"),
+          
           
         ],
       ),
